8d37893db348231a2a4dc45df6a6b4a36cc53dd1
yashodeep <yashodeepchikte@gmail.com>
2020-05-12 16:57:29 +0530

    all routes are working ... flash messages must be added

--- routes/articles.js
+++ routes/articles.js
@@ -7,104 +7,129 @@ const Articles = require("../models/articles.model");
 
 
 router.get("/", (req, res) => {
-    res.redirect("/")
+  res.redirect("/")
 })
+
+
+
+
 
 // creating a new article
 router.get("/new", (req, res) => {
-    res.render("articles/new", {article:new Articles()});
+  res.render("articles/new", {
+    article: new Articles()
+  });
 })
-
 
 router.get("/:slug", (req, res) => {
-    const slug = req.params.slug;
-    // console.log("sslug: " , slug)
-    Articles.findOne({slug:slug}, (err, result) => {
-        if (result == null){res.redirect("/")}
-        // console.log("result --> " , result)
-        res.render("articles/show", {article:result})
+  const slug = req.params.slug;
+  // console.log("sslug: " , slug)
+  Articles.findOne({
+    slug: slug
+  }, (err, result) => {
+    if (result == null) {
+      res.redirect("/")
+    }
+    // console.log("result --> " , result)
+    res.render("articles/show", {
+      article: result
     })
+  })
 })
+
 
 
 router.post("/neww", async (req, res) => {
-    console.log('got to the new route')
-    // console.log("request=", req.body)
-    let article = new Articles({
-        title:req.body.title,
-        description:req.body.description,
-        markdown:req.body.markdown
-    })
-    try{
+  // console.log('got to the new route')
+  // console.log("request=", req.body)
+
+  Articles.find({
+    title: req.body.title
+  }, async (err, docs) => {
+
+    if (docs.length == 0) {
+      let article = new Articles({
+        title: req.body.title,
+        description: req.body.description,
+        markdown: req.body.markdown
+      })
+      try {
         await article.save();
         // console.log("articel", article);
         res.redirect(`${article.slug}`)
-    }catch(err){
-        console.log(err);
-        res.render("/new", {article:article});
+      } catch (err) {
+        res.write("An item with teh same title already exists")
+        res.redirect("/new");
+      }
+    } else {
+
+      req.body.title += String(docs.length);
+
+      let article = new Articles({
+        title: req.body.title,
+        description: req.body.description,
+        markdown: req.body.markdown
+      })
+      try {
+        await article.save();
+        // console.log("articel", article);
+        res.redirect(`${article.slug}`)
+      } catch (err) {
+        res.write("An item with teh same title already exists")
+        res.redirect("/new");
+
+      }
     }
-});
+
+  });
+
+})
 
 
 
-    // console.log("req was", req.body);
+// console.log("req was", req.body);
 
 
 
 //  Deleting an element
 router.get("/delete/:slug", async (req, res) => {
-   const slug = req.params.slug;
-   console.log(slug, "Deleted")
-   Articles.findOneAndDelete({slug:slug}, (err)=>{
+  const slug = req.params.slug;
+  // console.log(slug, "Deleted")
+  Articles.findOneAndDelete({
+    slug: slug
+  }, (err) => {
     //    Articles.save();
-       res.redirect("/")
-   })
+    res.redirect("/")
+  })
 
 })
 
 
 // Editing an element
 router.get("/edit/:slug", (req, res) => {
-    const slug = req.params.slug;
-    // console.log("slug:", slug)
-    Articles.findOne({slug:slug}, (e, result) =>
-    {
-        res.render("articles/edit", {article:result})
+  const slug = req.params.slug;
+  // console.log("slug:", slug)
+  Articles.findOne({
+    slug: slug
+  }, (e, result) => {
+    res.render("articles/edit", {
+      article: result
     })
+  })
 })
 
 router.post("/edit", async (req, res) => {
-    const id = req.body.id;
+  const id = req.body.id;
 
-    var article =  await  Articles.findOne({_id:id})
-    article.title=req.body.title;
-    article.description = req.body.description;
-    article.markdown = req.body.markdown;
-    // article.createdAt = Date.now();
-    await article.save();
-    res.redirect("/")
+  var article = await Articles.findOne({
+    _id: id
+  })
+  article.title = req.body.title;
+  article.description = req.body.description;
+  article.markdown = req.body.markdown;
+  // article.createdAt = Date.now();
+  await article.save();
+  res.redirect("/")
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 module.exports = router;
